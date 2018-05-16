62b8b90bead7e3cea630ae925a32e11546e665ac
yad <yad@yad.com>
2018-05-16 17:36:22 +0200

    feat: add first sln with some quests

---
+++ LegacyWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace LegacyWebApplication.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content(string.Format("Welcome {0} !", HttpContext.User.Identity.Name));
+        }
+    }
+}
